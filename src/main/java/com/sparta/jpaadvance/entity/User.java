d40996f2c5a9bf5e2f7cf8524772ae9df81ac6a0
Sun choi <csun2772@naver.com>
2024-08-21 19:11:07 +0900

    @ManyToOne 단방향 테스트

--- src/main/java/com/sparta/jpaadvance/entity/User.java
+++ src/main/java/com/sparta/jpaadvance/entity/User.java
@@ -14,13 +14,4 @@ public class User {
     private Long id;
     private String name;
 
-    @OneToOne(mappedBy = "user")
-    private Food food;
-
-    public void addFood(Food food) {
-        this.food = food;
-        food.setUser(this);
-        // This 는 객체 자신
-        //객체 자신을 외래키주인에게 주게 됨
-    }
 }
